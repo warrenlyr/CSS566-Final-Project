6348474e89294b5c61de2095011c486374cb3c58
Arsheya Raj <raj94arsheya@gmail.com>
2023-05-07 17:31:37 -0700

    Added the leaderboard and buttons component in landing page

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
-import LandingPage from "./pages/landing";
-import GamePage from "./pages/game";
-import CustomizePage from "./pages/customize";
+import LandingPage from "./pages/Landing Page/landing";
+import GamePage from "./pages/Today's Rewards Game/game";
+import CustomizePage from "./pages/User Puzzle Design/customize";
 import NotFound from "./pages/notFound";
 import Register from "./pages/register";
 
