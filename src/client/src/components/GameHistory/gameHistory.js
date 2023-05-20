61da639830f4913dbdc4b6362ac810cb1cc403e7
Barack <yanghongliu2013@outlook.com>
2023-05-19 19:28:16 -0700

    Call backend API to read user history

--- src/client/src/components/GameHistory/gameHistory.js
+++ src/client/src/components/GameHistory/gameHistory.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import "./styles.scss";
-import { apiInstance } from "../../services/apiInstance";
+import { authApiInstance } from "../../services/authApiInstance";
 
-const GameHistory = ({ styles, accessToken }) => {
+const GameHistory = ({ styles}) => {
 	const [gameHistoryData, setGameHistoryData] = useState([]);
 
 	useEffect(() => {
@@ -12,12 +12,8 @@ const GameHistory = ({ styles, accessToken }) => {
 	const fetchData = async () => {
 		const path = "/auth/user/gamehistory";
 
-		await apiInstance
-			.get(path, {
-				headers: {
-					"Authorization": `Bearer ${accessToken}`
-				}
-			})
+		await authApiInstance
+			.get(path)
 			.then((res) => {
 				const data = res.data;
 				console.log(data);
@@ -32,12 +28,26 @@ const GameHistory = ({ styles, accessToken }) => {
 		<div className={`gameHistoryContainer ${styles}`}>
 			<div className="gameHistoryTitle">User History</div>
 			<table>
+				<thead>
+					<tr>
+						<th>Game Name</th>
+						<th>Start Time</th>
+						<th>End Time</th>
+						<th>Finished</th>
+						<th>Attempts</th>
+						<th>Score</th>
+					</tr>
+				</thead>
 				<tbody>
 					{gameHistoryData.map((game) => {
 						return (
 							<tr className="gameHistoryItem" key={game.game_history_id}>
-								<td className="item">{game.game_name}</td>
-								<td className="detail">{`Start TIme: ${game.start_time}, End Time: ${game.end_time}`}</td>
+								<td>{game.game_name}</td>
+								<td>{game.start_time}</td>
+								<td>{game.end_time ? game.end_time : "Still playing"}</td>
+								<td>{game.finished ? "Yes" : "No"}</td>
+								<td>{game.attempts}</td>
+								<td>{game.score}</td>
 							</tr>
 						);
 					})}
--- src/client/src/pages/Landing Page/landing.js
+++ src/client/src/pages/Landing Page/landing.js
@@ -8,7 +8,7 @@ import GameHistory from "../../components/GameHistory/gameHistory";
 const LandingPage = () => {
 	return (
 		<>
-			<div className="pageTitle">Word Game Name</div>
+			<div className="pageTitle">Husky seeks for Kitty</div>
 			<div className="landingContainer">
 				<Leaderboard
 					styles={"landingLeaderboard"}
@@ -37,5 +37,7 @@ const LandingPage = () => {
 		</>
 	);
 };
+
+
 
 export default LandingPage;
