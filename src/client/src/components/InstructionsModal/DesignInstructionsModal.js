593f75536d6b0c0e62cb3c54ab4473cc66b4cb4b
Arsheya Raj <raj94arsheya@gmail.com>
2023-05-25 16:38:40 -0700

    Updated Instructions for Normal, Daily Puzzle Page

--- src/client/src/components/InstructionsModal/DesignInstructionsModal.js
+++ src/client/src/components/InstructionsModal/DesignInstructionsModal.js
@@ -7,8 +7,13 @@ const DesignInstructionsModal = ({ isShowing, hide }) => isShowing ? ReactDOM.cr
 		<div className="modal-overlay"/>
 		<div className="modal-wrapper" aria-modal aria-hidden tabIndex={-1} role="dialog">
 			<div className="modal">
+				< h1 > Welcome to the Design Puzzle Page! </h1>
+				<p>The instructions for the game are as follows:</p>
+				<ol>
+					<li>Select the Level from the dropdown as level 1:5x5 grid, level 2:7x7 grid, or level 3:10x10 grid.</li>
+					<li>Click the Clear button to clear all the selections from the grid.</li>
+				</ol>
 				<div className="modal-header">
-					< h1 > Hello, Im a modal. </h1>
 					<button type="button" className="modal-close-button" data-dismiss="modal" aria-label="Close" onClick={hide}>
 						<span aria-hidden="true">&times;</span>
 					</button>
