bfa5b7cb2ae93a54539de9aca767ccb46cf4222e
Haihan Jiang <git config --global --replace-all user.email>
2023-05-24 18:28:33 -0700

    GameGrid.test.js

---
+++ src/client/src/__tests__/GameGrid.test.js
@@ -0,0 +1,65 @@
+import { render, screen, waitFor, fireEvent } from '@testing-library/react';
+import GameGrid from '../components/GameGrid/GameGrid';
+import React from "react";
+import { MemoryRouter as Router } from "react-router-dom";
+
+describe("GameGrid Component", () => {
+
+  const mockProps = {
+    puzzle: [
+      ['a', 'b', 'c'],
+      ['d', 'e', 'f'],
+      ['g', 'h', 'i']
+    ],
+    size: 3,
+    words: ['abc', 'def', 'ghi'],
+    level: 1,
+    gameId: 1,
+    gameHistoryId: 1,
+    token: 'mockToken',
+  };
+  
+  test('renders GameGrid component', async () => {
+    render(
+      <Router>
+        <GameGrid {...mockProps} />
+      </Router>
+    );
+    await waitFor(() => expect(screen.getByText('WORDS TO FIND')).toBeInTheDocument());
+  });
+
+  test("should display correct game level", async () => {
+    // If the component doesn't display the game level directly, you may need to mock this functionality
+    // or adjust the test to suit your application.
+  });
+
+  test("should render the puzzle grid correctly", async () => {
+    const { container } = render(
+      <Router>
+        <GameGrid {...mockProps} />
+      </Router>
+    );
+    await waitFor(() => {
+      const rows = container.querySelectorAll('.boardRow');
+      expect(rows.length).toBe(3);
+
+      const cells = container.querySelectorAll('.square');
+      expect(cells.length).toBe(mockProps.size * mockProps.size); // Ensure the correct number of cells are present
+    });
+  });
+
+  test("should trigger the correct action on cell selection", async () => {
+    const { container } = render(
+      <Router>
+        <GameGrid {...mockProps} />
+      </Router>
+    );
+    await waitFor(() => {
+        const firstCell = container.querySelector('.square');
+        fireEvent.click(firstCell);
+        // You'll need to add the logic in your code to handle cell selection and test this 
+        // Below line may vary depending on your actual implementation
+        // expect(firstCell).toHaveClass('selected');
+      });
+  });
+});
